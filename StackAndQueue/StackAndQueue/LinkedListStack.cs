eff7b167861a004103a7e1c543f703c02718243c
“Love <opera.prem@gmail.com>
2022-08-21 08:54:20 +0430

    ADDED: Add and Display Method

---
+++ StackAndQueue/StackAndQueue/LinkedListStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class LinkedListStack
+    {
+        internal Node top;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is pusheed into the stack", node.data);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine("\n" + temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Welcome To Stack And Queues Program\n");
+﻿using System;
+using StackAndQueue;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack And Queues Program\n");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Add(70);
+            stack.Add(30);
+            stack.Add(56);
+            Console.WriteLine("\nPushed 56 on top of the stack");
+            stack.Display();
+            Console.ReadLine();
+        }
+    }
+}
